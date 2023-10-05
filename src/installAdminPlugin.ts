62ee887fa48b5be0a992e8547f26205779a2e131
pythonpete32 <aaron@blackscale.io>
2023-10-05 10:49:21 +0400

    refactor: prepare with the SDK

--- src/installAdminPlugin.ts
+++ src/installAdminPlugin.ts
@@ -4,7 +4,7 @@ import {
     activeContractsList,
 } from '@aragon/osx-ethers';
 import { ethers } from 'ethers';
-import { ApplyInstallationParams, DaoAction } from '@aragon/sdk-client-common';
+import { ApplyInstallationParams, DaoAction, MetadataAbiInput, PrepareInstallationParams, PrepareInstallationStep, PrepareInstallationStepValue } from '@aragon/sdk-client-common';
 import { ProposalCreationSteps, VoteValues } from '@aragon/sdk-client';
 import { client, tokenVotingClient } from './lib/sdk';
 import { getWallet, hexToBytes, getLogFromReceipt } from './lib/helpers';
@@ -25,18 +25,10 @@ const deployer = getWallet();
 const aragonClient = client(NETWORK);
 const votingClient = tokenVotingClient(NETWORK);
 
-// This is the contract that prepares and actually installs the plugin. You can think of this as 
-// homebrew, pacman, or apt-get. It's the thing that actually installs the plugin.
-const PluginSetupProcessor = activeContractsList[NETWORK]["PluginSetupProcessor"];
 
 // We are going to use the admin repo because its super simple and deploying our own repo can already be done with the cli
 const adminRepoAddress = activeContractsList[NETWORK]["admin-repo"]
 
-// ***Contract instances***
-const setupProcessor = PluginSetupProcessor__factory.connect(PluginSetupProcessor, deployer);
-const pspInterface = PluginSetupProcessor__factory.createInterface();
-
-// ***0b. Setup your DAO stuff***
 
 // get the dao details
 const daoDetails = await aragonClient.methods.getDao(DAO_ADDRESS_OR_ENS)
@@ -54,64 +46,61 @@ log("Voting Plugin: ", VOTING_APP_ADDRESS)
 // https://github.com/aragon/osx/blob/a52bbae69f78e74d6a17647370ccfa2f2ea9bbf0/packages/contracts/src/framework/plugin/setup/PluginSetupProcessor.sol#L287-L288
 // ==============================================================
 
+// 1a. ***Prepare the installation metadata***
+// This is the metadata that is needed to initialize the plugin. Its the the same thing that is encoded in the setup contract
+// https://devs.aragon.org/docs/osx/how-to-guides/plugin-development/publication/metadata
+const adminSetupAbiMetadata: MetadataAbiInput[] = [
+    {
+        "internalType": "address",
+        "name": "member",
+        "type": "address",
+        "description": "The address of the initial admin."
+    },
+]
 
-// 1a. ***Encode the installation data***
-// Here we are encoding the data that is needed to initialize the plugin. Its the the same thing that is decoded here in the setup contract
-// https://github.com/aragon/osx/blob/1c7b8ecbac9df80879c9c23d3409d50a1960e403/packages/contracts/src/plugins/governance/admin/AdminSetup.sol#L38-L39
-const prepareInstallationData = ethers.utils.defaultAbiCoder.encode(
-    ['address'],
-    [deployer.address]
-);
-
-
-// 1b. ***Prepare the installation***
-// the pluginSetupRef is the reference to the plugin setup to be used for the installation. Remember PluginRepos are just contracts that version control plugins
-// so the pluginSetupRef is the reference to the plugin setup to be used for the installation.
-const pluginSetupRef = {
-    versionTag: { release: 1, build: 1 },
-    pluginSetupRepo: adminRepoAddress,
-}
-
+// 1b. ***Prepare the installation params***
+const adminSetupParams = [deployer.address]
 
 // 1c. ***Prepare the installation***
-// putting it all together we can now prepare the installation
-const prepareInstallationParams = {
-    pluginSetupRef,
-    data: hexToBytes(prepareInstallationData),
+const prepareInstallParams: PrepareInstallationParams = {
+    daoAddressOrEns: DAO_ADDRESS,
+    pluginRepo: adminRepoAddress,
+    installationAbi: adminSetupAbiMetadata,
+    installationParams: adminSetupParams,
 }
 
-// 1d. ***Call the prepareInstallation() on the PSP **
-const prepareInstallationTx = await setupProcessor.prepareInstallation(DAO_ADDRESS, prepareInstallationParams);
-const prepareInstallationReceipt = await prepareInstallationTx.wait();
-const prepInstallLog = await getLogFromReceipt(prepareInstallationReceipt, pspInterface, 'InstallationPrepared');
+log("Prepare Installation...")
+// 1d. ***Call the prepareInstallation() on the SDK **
+// This returns an async generator that will return the steps as they are completed
+const prepareSteps = aragonClient.methods.prepareInstallation(prepareInstallParams)
 
-// here we are getting the params from the log. we care about the plugin address and the permissions
-const { plugin: pluginAddress, preparedSetupData: { permissions } } = prepInstallLog;
+// 1e. ***Iterate through the steps***
+const prepareInstallStep1 = await (await prepareSteps.next()).value;
+log("Transaction Hash: ", prepareInstallStep1.txHash);
+
+const prepareInstallStep2 = await (await prepareSteps.next()).value;
+log("Installation Data: ", prepareInstallStep2);
+
+// this is already an object that has all the data we need to apply the installation. it also has the Key from the iterator but we dont need that
+const installdata = prepareInstallStep2 satisfies ApplyInstallationParams
+
 
 
 // ==============================================================
-// 3. Create Proposal to Apply install: Using the PluginSetupProcessor, use the SDK to get the set of actions and create a proposal
+// 2. Create Proposal to Apply install: Using the PluginSetupProcessor, use the SDK to get the set of actions and create a proposal
 // https://github.com/aragon/osx/blob/a52bbae69f78e74d6a17647370ccfa2f2ea9bbf0/packages/contracts/src/framework/plugin/setup/PluginSetupProcessor.sol#L287-L288
 // ==============================================================
 
-// 3a. ***Get the ApplyInstallationParams***
-const applyInstallationParams: ApplyInstallationParams = {
-    helpers: [],
-    permissions,
-    pluginAddress,
-    pluginRepo: adminRepoAddress,
-    versionTag: { release: 1, build: 1 },
-}
 
 
-// 3b. ***Encode the actions***
+// 2a. ***Encode the actions***
 // Here we use the client to create the encoded actions. This creates 3 actions, 
 // [0] Grants the PSP permission to install, 
 // [1] Installs the plugin, 
 // [2] Removes the PSP permission to install
-const daoActions: DaoAction[] = aragonClient.encoding.applyInstallationAction(DAO_ADDRESS, applyInstallationParams);
+const daoActions: DaoAction[] = aragonClient.encoding.applyInstallationAction(DAO_ADDRESS, installdata);
 
-// 3c. ***Pin the metadata***
+// 2b. ***Pin the metadata***
 const metadataUri: string = await votingClient.methods.pinMetadata({
     title: "Test metadata",
     summary: "This is a test proposal",
@@ -128,9 +117,9 @@ const metadataUri: string = await votingClient.methods.pinMetadata({
     },
 });
 
-// 3d. ***Create the proposal***
+// 2c. ***Create the proposal***
 // this returns an export generator that will create the proposal
-const steps = votingClient.methods.createProposal({
+const createProposalSteps = votingClient.methods.createProposal({
     metadataUri,
     pluginAddress: VOTING_APP_ADDRESS,
     actions: daoActions,
@@ -140,19 +129,11 @@ const steps = votingClient.methods.createProposal({
     endDate: new Date(0), // uses minimum voting duration
 })
 
-// 3e. ***Iterate through the steps***
-for await (const step of steps) {
-    try {
-        switch (step.key) {
-            case ProposalCreationSteps.CREATING:
-                log("Transaction Hash: ", step.txHash);
-                break;
-            case ProposalCreationSteps.DONE:
-                log("Proposal ID", step.proposalId);
-                break;
-        }
-    } catch (err) {
-        console.error(err);
-    }
-    log("🎉🎉🎉🎉🎉🎉🎉🎉🎉");
-}
+
+
+// 2d. ***Iterate through the steps***
+const createProposalStep1Value = await (await createProposalSteps.next()).value;
+log("Transaction Hash: ", await createProposalStep1Value.txHash);
+
+const createProposalStep2Value = await (await createProposalSteps.next()).value;
+log("Proposal ID: ", await createProposalStep2Value.proposalId);
