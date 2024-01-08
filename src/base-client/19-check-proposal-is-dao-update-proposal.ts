494efbbf48dbacce7684ebb6b6464543b3880d11
pythonpete32 <aaron@DAOresear.ch>
2024-01-08 15:34:00 +0400

    feat: :zap: add base client examples

---
+++ src/base-client/19-check-proposal-is-dao-update-proposal.ts
@@ -0,0 +1,12 @@
+import { createClient } from "../lib/sdk";
+import { PrepareUninstallationParams, GasFeeEstimation } from "@aragon/sdk-client-common";
+const client = await createClient("polygon")
+
+
+// The Id of the proposal
+const proposalId: string = "0x0cff359a7455de5bb50aa0567517536d3dfe002d_0x11"
+
+// check if a plugin update proposal is valid
+const isValid = client.methods.isDaoUpdateProposal(proposalId);
+
+console.log(await isValid)
